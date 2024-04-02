6af88a5d02b05f995993df42a7d4b294ef0dbaf7
Lebing Xie <pingp76@yahoo.com>
2024-04-02 21:38:05 +0800

    pass all the tests, major functions are ready

--- programs/wu-pay-solana/src/lib.rs
+++ programs/wu-pay-solana/src/lib.rs
@@ -8,6 +8,14 @@ declare_id!("Do7Ta4RFCwrBhbESGcMg6W7sLNqXMcq5E7YV5CAEZhij");
 const SEEDSTATE:&[u8] = b"escrow_state";
 const SEEDWALLET:&[u8] = b"escrow_wallet";
 
+#[error_code]
+pub enum ErrorCode {
+    #[msg("The amount to withdrwa does not match the amount in the escrow state")]
+    InvalidAmount,
+    #[msg("The bump seed for the wallet does not match the escrow state's wallet bump seed")]
+    InvalidWalletBump,
+}
+
 #[program]
 pub mod wu_pay_solana {
 
@@ -18,7 +26,7 @@ pub mod wu_pay_solana {
         escrow_state.sender = _ctx.accounts.sender.key();
         escrow_state.receiver = _ctx.accounts.receiver.key();
         escrow_state.amount = _amount;
-        escrow_state.waller_bump = _wallet_bump;
+        escrow_state.wallet_bump = _wallet_bump;
         let system_program = &mut _ctx.accounts.system_program;
 
         let transfer_instruction = anchor_lang::system_program::Transfer {
@@ -30,19 +38,33 @@ pub mod wu_pay_solana {
             system_program.to_account_info(),
             transfer_instruction);
         
-            anchor_lang::system_program::transfer(cpictx, _amount)?;
+        anchor_lang::system_program::transfer(cpictx, _amount)?;
         Ok(())
     }
 
-    pub fn complete_grant(_ctx: Context<DepositeGrant>, _deposite_idx:u64, _state_bump:u8, _wallet_bump:u8, _amount:u64) -> Result<()> {
-        //let escrow_state = &mut _ctx.accounts.escrow_state;
+    pub fn complete_grant(_ctx: Context<CompleteGrant>, _deposite_idx:u64, _state_bump:u8, _wallet_bump:u8, _amount:u64) -> Result<()> {
+        let escrow_state = &mut _ctx.accounts.escrow_state;
         let escrow_wallet = &mut _ctx.accounts.escrow_wallet;
         let system_program = &mut _ctx.accounts.system_program;
-        let sender = &mut _ctx.accounts.sender;
+        let sender = &_ctx.accounts.sender;
         let receiver = &mut _ctx.accounts.receiver;
         let _deposite_idx_bytes = _deposite_idx.to_le_bytes();
         let _wallet_bump_bytes = _wallet_bump.to_le_bytes();
 
+        if escrow_state.amount != _amount {
+            return Err(ErrorCode::InvalidAmount.into());
+        }
+
+        if escrow_state.wallet_bump != _wallet_bump {
+            return Err(ErrorCode::InvalidWalletBump.into());
+        }
+
+        msg!("Transfering amount to receiver...");
+        **escrow_wallet.to_account_info().try_borrow_mut_lamports()? -= _amount;
+        **receiver.to_account_info().try_borrow_mut_lamports()? += _amount;
+
+        /* 
+        msg!("Creating seeds for escrow wallet and state...");
         let inner = vec![
             SEEDWALLET.as_ref(),
             sender.key.as_ref(),
@@ -63,49 +85,40 @@ pub mod wu_pay_solana {
             transfer_instruction,
             outer.as_slice(),
         );
-
+        msg!("Transferring amount to receiver...");
         anchor_lang::system_program::transfer(cpictx, _amount)?;
+        */
         Ok(())
     }
 
-}
+    pub fn withdraw_grant(_ctx: Context<WithdrwaGrant>, _deposite_idx:u64, _state_bump:u8, _wallet_bump:u8, _amount:u64) -> Result<()> {
+        let escrow_state = &mut _ctx.accounts.escrow_state;
+        let escrow_wallet = &mut _ctx.accounts.escrow_wallet;
+        let system_program = &mut _ctx.accounts.system_program;
+        let sender = &_ctx.accounts.sender;
+        let receiver = &mut _ctx.accounts.receiver;
+        let _deposite_idx_bytes = _deposite_idx.to_le_bytes();
+        let _wallet_bump_bytes = _wallet_bump.to_le_bytes();
+
+        if escrow_state.amount != _amount {
+            return Err(ErrorCode::InvalidAmount.into());
+        }
+
+        if escrow_state.wallet_bump != _wallet_bump {
+            return Err(ErrorCode::InvalidWalletBump.into());
+        }
+
+        msg!("Withdraw amount to sender...");
+        **escrow_wallet.to_account_info().try_borrow_mut_lamports()? -= _amount;
+        **sender.to_account_info().try_borrow_mut_lamports()? += _amount;
+        Ok(())
+    }
 
 
-#[derive(Accounts)]
-#[instruction(deposite_idx:u64, state_bump:u8, wallet_bump:u8)]
-pub struct CompleteGrant<'info> {
-    #[account(mut)]
-    sender: Signer<'info>,
-    /// CHECK: this is safe, will not write to this account
-    receiver: AccountInfo<'info>, 
+    pub fn close_escrow(_ctx: Context<CloseEscrow>, _deposite_idx:u64, _state_bump:u8, _wallet_bump:u8) -> Result<()> {
+        Ok(())
+    }
 
-    #[account(
-        mut,
-        seeds = [
-            SEEDSTATE,
-            sender.key.as_ref(),
-            receiver.key.as_ref(),
-            deposite_idx.to_le_bytes().as_ref(),
-        ],
-        bump = state_bump,
-        has_one = sender,
-        has_one = receiver,
-    )]
-    escrow_state: Account<'info, EscrowState>,
-
-    #[account(
-        mut,
-        seeds = [
-            SEEDWALLET,
-            sender.key.as_ref(),
-            receiver.key.as_ref(),
-            deposite_idx.to_le_bytes().as_ref(),
-        ],
-        bump = wallet_bump,
-    )]
-    escrow_wallet: SystemAccount<'info>,
-
-    system_program: Program<'info, System>,
 }
 
 
@@ -122,7 +135,7 @@ pub struct DepositeGrant<'info> {
     #[account(
         init,
         payer = sender,
-        space = 8 + 8 + 8 + 1,
+        space = EscrowState::LEN,
         seeds = [
             SEEDSTATE,
             sender.key.as_ref(),
@@ -136,7 +149,7 @@ pub struct DepositeGrant<'info> {
     #[account(
         init,
         payer = sender,
-        space = 8 + 8 + 1,
+        space = EscrowWallet::LEN,
         seeds = [
             SEEDWALLET,
             sender.key.as_ref(),
@@ -150,13 +163,129 @@ pub struct DepositeGrant<'info> {
     system_program: Program<'info, System>,
 }
 
+#[derive(Accounts)]
+#[instruction(deposite_idx:u64, state_bump:u8, wallet_bump:u8)]
+pub struct CompleteGrant<'info> {
+    /// CHECK: this is safe, will not write to this account
+    sender: Signer<'info>,
+    
+    /// CHECK: this is safe, will not write to this account
+    #[account(mut)]
+    receiver: AccountInfo<'info>, 
+
+    #[account(
+        mut,
+        seeds = [
+            SEEDSTATE,
+            sender.key.as_ref(),
+            receiver.key.as_ref(),
+            deposite_idx.to_le_bytes().as_ref(),
+        ],
+        bump = state_bump,
+    )]
+    escrow_state: Account<'info, EscrowState>,
+
+    #[account(
+        mut,
+        seeds = [
+            SEEDWALLET,
+            sender.key.as_ref(),
+            receiver.key.as_ref(),
+            deposite_idx.to_le_bytes().as_ref(),
+        ],
+        bump = wallet_bump,
+    )]
+    escrow_wallet: Account<'info,EscrowWallet>,
+
+    system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(deposite_idx:u64, state_bump:u8, wallet_bump:u8)]
+pub struct WithdrwaGrant<'info> {
+    /// CHECK: this is safe, will not write to this account
+    #[account(mut)]
+    sender: Signer<'info>,
+    
+    /// CHECK: this is safe, will not write to this account
+    receiver: AccountInfo<'info>, 
+
+    #[account(
+        mut,
+        seeds = [
+            SEEDSTATE,
+            sender.key.as_ref(),
+            receiver.key.as_ref(),
+            deposite_idx.to_le_bytes().as_ref(),
+        ],
+        bump = state_bump,
+    )]
+    escrow_state: Account<'info, EscrowState>,
+
+    #[account(
+        mut,
+        seeds = [
+            SEEDWALLET,
+            sender.key.as_ref(),
+            receiver.key.as_ref(),
+            deposite_idx.to_le_bytes().as_ref(),
+        ],
+        bump = wallet_bump,
+    )]
+    escrow_wallet: Account<'info,EscrowWallet>,
+    system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(deposite_idx:u64, state_bump:u8, wallet_bump:u8)]
+pub struct CloseEscrow<'info> {
+    /// CHECK: this is safe, will not write to this account
+    #[account(mut)]
+    sender: Signer<'info>,
+    
+    /// CHECK: this is safe, will not write to this account
+    receiver: AccountInfo<'info>, 
+
+    #[account(
+        mut,
+        seeds = [
+            SEEDSTATE,
+            sender.key.as_ref(),
+            receiver.key.as_ref(),
+            deposite_idx.to_le_bytes().as_ref(),
+        ],
+        close=sender,
+        bump = state_bump,
+    )]
+    escrow_state: Account<'info, EscrowState>,
+
+    #[account(
+        mut,
+        seeds = [
+            SEEDWALLET,
+            sender.key.as_ref(),
+            receiver.key.as_ref(),
+            deposite_idx.to_le_bytes().as_ref(),
+        ],
+        close=sender,
+        bump = wallet_bump,
+    )]
+    escrow_wallet: Account<'info,EscrowWallet>,
+    system_program: Program<'info, System>,
+}
+
+
 #[account]
 #[derive(Default)]
 pub struct EscrowState {
     pub sender: Pubkey,
     pub receiver: Pubkey,
     pub amount: u64,
-    waller_bump: u8,
+    wallet_bump: u8,
+}
+
+impl EscrowState {
+    pub const LEN:usize = 8 + 32 + 32 + 8 + 1;
 }
 
 #[account]
@@ -164,4 +293,7 @@ pub struct EscrowState {
 pub struct EscrowWallet {
 }
 
+impl EscrowWallet {
+    pub const LEN:usize = 8;
+}
 
--- tests/wu-pay-solana.ts
+++ tests/wu-pay-solana.ts
@@ -20,88 +20,142 @@ describe("wu-pay-solana", () => {
   const provider = anchor.getProvider()
   const program = anchor.workspace.WuPaySolana as Program<WuPaySolana>;
 
-  // create test accounts
+  // create testing accounts, including sender, reciever, and escrow wallet PDAs
   let alice: anchor.web3.Keypair;
   let bob: anchor.web3.Keypair;
   let pda: PDAParameters;
 
+  // initialize the testing accounts before each test
   beforeEach(async () => {
     alice = await createUserAndAirdrop(5, provider);
-    bob = await createUserAndAirdrop(1, provider);
+    bob = await createUserAndAirdrop(0, provider);
     pda = await createPdaParams(provider.connection, alice.publicKey, bob.publicKey);
   });
 
-  it("Case1: Normal case, alice trasfer 20 to bob via safetybox", async () => {
-    console.log(`Step1: Initialized a new Safe Pay instance. Alice sent 20 tokens to saftybox`);
+  // The main test case
+  it("Case1: Normal case, alice trasfer 20 to bob via Escrow wallet", async () => {
+    console.log(`Step1: Initialized a new  Escrow wallet PDA. Alice sent 20 tokens to saftybox`);
+    // Assert initial balances
     const aliceBalancePre = await readAccount(alice.publicKey, provider);
     assert.equal(aliceBalancePre, '5000000000');
-
+    // amount to transfer
     const amount = new anchor.BN(20000000);
 
-    // Step1: Alice sent 20 tokens to saftybox
-    const tx1 = await program.methods.depositeGrant(pda.idx, pda.statebump, pda.walletbump,amount).accounts({
+    // Step1: Alice sent 20 tokens to Escrow wallet
+    const tx1 = await program.methods.depositeGrant(pda.idx, pda.statebump, pda.walletbump, amount).accounts({
       sender: alice.publicKey,
       receiver: bob.publicKey,
       escrowState: pda.statekey,
       escrowWallet: pda.walletkey,
       systemProgram: anchor.web3.SystemProgram.programId,
     }).signers([alice]).rpc(
-      {skipPreflight:true}
+      //{skipPreflight:true}
     );
 
     // Assert that 20 tokens were moved from Alice's account to the escrow.
     const aliceBalancePost = await readAccount(alice.publicKey, provider);
-    console.log("Alice's balance: {}", aliceBalancePost);
-    assert.equal(aliceBalancePost, '4978413120');
+    console.log("Alice's balance: ", aliceBalancePost);
+    assert.equal(aliceBalancePost, '4977598800'); // 20 tokens + rent for PDA
+
     const saftyboxBalancePost = await readAccount(pda.walletkey, provider);
-    console.log("Safetybox's balance: {}", saftyboxBalancePost);
-    assert.equal(saftyboxBalancePost, '21586880');
+    console.log("Escrow Wallet's balance: ", saftyboxBalancePost);
+    assert.equal(saftyboxBalancePost, '20946560'); // 20 tokens + rent for PDA
 
     //Step2: Complete the grant. Bob received 20 tokens from saftybox
     console.log(`Step2: Complete the grant. Bob received 20 tokens from saftybox`);
-    try{
- 
-    const tx2 = await program.methods.completeGrant(pda.idx, pda.statebump, pda.walletbump,amount).accounts({
+
+    const tx2 = await program.methods.completeGrant(pda.idx, pda.statebump, pda.walletbump, amount).accounts({
       sender: alice.publicKey,
       receiver: bob.publicKey,
       escrowState: pda.statekey,
       escrowWallet: pda.walletkey,
       systemProgram: anchor.web3.SystemProgram.programId,
     }).signers([alice]).rpc(
-      {skipPreflight:true}
+      //{ skipPreflight: true }
     );
-  }catch(error){
-    console.error('Error message:', error.message);
-    console.error('Stack trace:', error.stack);
-  }
 
     // Assert that 20 tokens were sent to bob.
     const bobBalance = await readAccount(bob.publicKey, provider);
-    console.log("Bob's balance: {}", bobBalance);
-    const saftyboxBalancePost2 = await readAccount(pda.walletkey, provider);
-    console.log("Safetybox's balance: {}", saftyboxBalancePost2);
+    console.log("Bob's balance at end: ", bobBalance);
     assert.equal(bobBalance, '20000000');
-    console.log(`Step3: Clean up. Close the saftybox account.`);
+    const saftyboxBalancePost2 = await readAccount(pda.walletkey, provider);
+    console.log("Escrow wallet's balance at end: ", saftyboxBalancePost2);
+    const aliceBalancePost3 = await readAccount(alice.publicKey, provider);
+    console.log("Alice's balance at end: ", aliceBalancePost3);
 
-    /*
-    const tx3 = await program.methods.closePda(pda.idx, pda.bump).accounts({
-      fromPayer: alice.publicKey,
-      toPayee: bob.publicKey,
-      saftyboxPda: pda.saftyboxKey,
-      //systemProgram: anchor.web3.SystemProgram.programId,
+    console.log(`Step3: Clean up. Close the PDA account.`);
+    //Step3: Clean up. Close the PDA account.
+    const tx3 = await program.methods.closeEscrow(pda.idx, pda.statebump, pda.walletbump,).accounts({
+      sender: alice.publicKey,
+      receiver: bob.publicKey,
+      escrowState: pda.statekey,
+      escrowWallet: pda.walletkey,
+      systemProgram: anchor.web3.SystemProgram.programId,
     }).signers([alice]).rpc();
-
-
-    // // Assert that saftybox was correctly closed.
+    // Assert that escrow wallet was correctly closed.
     try {
-      await readAccount(pda.saftyboxKey, provider);
+      //await readAccount(pda.statekey, provider);
+      await readAccount(pda.walletkey, provider);
       return assert.fail("Account should be closed");
     } catch (e) {
-      assert.equal(e.message, "Cannot read properties of null (reading 'data')");
+      assert.equal(e.message, "Account should be closed");
     }
-
-*/
   });
+
+
+  it("Case2: Send and withdraw", async () => {
+    console.log(`Step1: Initialized a new Escrow wallet PDA. Alice sent 20 tokens to saftybox`);
+    // Assert initial balances
+    const aliceBalancePre = await readAccount(alice.publicKey, provider);
+    assert.equal(aliceBalancePre, '5000000000');
+    // amount to transfer
+    const amount = new anchor.BN(20000000);
+
+    // Step1: Alice sent 20 tokens to Escrow wallet
+    const tx1 = await program.methods.depositeGrant(pda.idx, pda.statebump, pda.walletbump, amount).accounts({
+      sender: alice.publicKey,
+      receiver: bob.publicKey,
+      escrowState: pda.statekey,
+      escrowWallet: pda.walletkey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    }).signers([alice]).rpc(
+      //{skipPreflight:true}
+    );
+
+    // Assert that 20 tokens were moved from Alice's account to the escrow.
+    const aliceBalancePost = await readAccount(alice.publicKey, provider);
+    console.log("Alice's balance: ", aliceBalancePost);
+    assert.equal(aliceBalancePost, '4977598800'); // 20 tokens + rent for PDA
+
+    const saftyboxBalancePost = await readAccount(pda.walletkey, provider);
+    console.log("Escrow Wallet's balance: ", saftyboxBalancePost);
+    assert.equal(saftyboxBalancePost, '20946560'); // 20 tokens + rent for PDA
+
+    //Step2: Withdraw the grant. Alice received back 20 tokens from escrow wallet
+    console.log(`Step2: Withdraw the grant. Alice received back 20 tokens from escrow wallet`);
+
+    const tx2 = await program.methods.withdrawGrant(pda.idx, pda.statebump, pda.walletbump, amount).accounts({
+      sender: alice.publicKey,
+      receiver: bob.publicKey,
+      escrowState: pda.statekey,
+      escrowWallet: pda.walletkey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    }).signers([alice]).rpc(
+      //{ skipPreflight: true }
+    );
+
+    // Assert that 20 tokens were sent to bob.
+    const bobBalance = await readAccount(bob.publicKey, provider);
+    console.log("Bob's balance at end: ", bobBalance);
+    assert.equal(bobBalance, '0');
+    const saftyboxBalancePost2 = await readAccount(pda.walletkey, provider);
+    console.log("Escrow wallet's balance at end: ", saftyboxBalancePost2);
+    const aliceBalancePost3 = await readAccount(alice.publicKey, provider);
+    console.log("Alice's balance at end: ", aliceBalancePost3);
+
+  });
+
+
 
   const createUserAndAirdrop = async (airdropAmount: number, provider: anchor.Provider): Promise<anchor.web3.Keypair> => {
     const user = anchor.web3.Keypair.generate();
@@ -138,21 +192,23 @@ describe("wu-pay-solana", () => {
   }
 
   const createPdaParams = async (connection: anchor.web3.Connection, alice: anchor.web3.PublicKey, bob: anchor.web3.PublicKey): Promise<PDAParameters> => {
+    // this is a unique identifier for this transaction
     const uid = new anchor.BN(parseInt((Date.now() / 1000).toString()));
     const uidBuffer = uid.toBuffer('le', 8);
 
+    // Create a PDA for the escrow state account, holding the escrow account's authority.
     let [statePubKey, statebump] = await anchor.web3.PublicKey.findProgramAddressSync(
       [Buffer.from("escrow_state"), alice.toBuffer(), bob.toBuffer(), uidBuffer], program.programId,
     );
-
+    // create a PDA for the escrow wallet account, holding the SOL
     let [walletkey, walletbump] = await anchor.web3.PublicKey.findProgramAddressSync(
       [Buffer.from("escrow_wallet"), alice.toBuffer(), bob.toBuffer(), uidBuffer], program.programId,
     );
-      console.log(`- PDA Wallet created: ${walletkey.toBase58()}`);
-      console.log(`- PDA state created: ${statePubKey.toBase58()}`);
-      console.log('idx is {}',uid);
-      console.log('walletbump is {}',walletbump);
-      console.log('statebump is {}',statebump);
+    console.log(`- PDA Wallet created: ${walletkey.toBase58()}`);
+    console.log(`- PDA state created: ${statePubKey.toBase58()}`);
+    console.log('idx is ', uidBuffer);
+    console.log('walletbump is ', walletbump);
+    console.log('statebump is ', statebump);
     return {
       statekey: statePubKey,
       walletkey: walletkey,
